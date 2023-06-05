c6ace28685e9481dc1f2ba43c0974a239cb607f9
MatthiasBlc <mat.bouloc@gmail.com>
2023-06-05 15:47:28 +0200

    add the Read

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import { createServer } from "node:http";
+import { findTodos } from "./functions/todos_storage.js";
 
 createServer(async (req, res) => {
   res.setHeader("content-Type", "application/json");
+  const todos = await findTodos();
+  res.write(JSON.stringify(todos));
   res.end();
 }).listen(3000);
