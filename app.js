49b4a5e9858cf492430c13c831de21bd52422eeb
MatthiasBlc <mat.bouloc@gmail.com>
2023-06-05 16:03:37 +0200

    add the Create

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 import { createServer } from "node:http";
-import { findTodos } from "./functions/todos_storage.js";
+import { json } from "node:stream/consumers";
+import { createTodo, findTodos } from "./functions/todos_storage.js";
 
 createServer(async (req, res) => {
   res.setHeader("content-Type", "application/json");
-  const todos = await findTodos();
-  res.write(JSON.stringify(todos));
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  if (url.pathname === "/todos") {
+    if (req.method === "GET") {
+      const todos = await findTodos();
+      res.write(JSON.stringify(todos));
+    } else if (req.method === "POST") {
+      const newTodo = await json(req);
+      const todo = await createTodo(newTodo);
+      // console.log(newTodo);
+      res.write(JSON.stringify(todo));
+    }
+  } else {
+    res.writeHead(404);
+  }
+
   res.end();
 }).listen(3000);
--- functions/todos_storage.js
+++ functions/todos_storage.js
@@ -1,4 +1,4 @@
-import { readFile } from "node:fs/promises";
+import { readFile, writeFile } from "node:fs/promises";
 
 const path = "storage/todos.json";
 
@@ -16,4 +16,16 @@ const path = "storage/todos.json";
 export async function findTodos() {
   const data = await readFile(path, "utf8");
   return JSON.parse(data);
+}
+
+/**
+ * @param {string} title
+ * @param {boolean} completed
+ * @return {Promise<Todo>}
+ */
+export async function createTodo({ title, completed = false }) {
+  const todo = { title, completed, id: Date.now() };
+  const todos = [todo, ...(await findTodos())];
+  await writeFile(path, JSON.stringify(todos));
+  return todo;
 }
